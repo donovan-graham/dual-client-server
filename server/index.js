58585f723fa3446a4eb1bfdecbd9173fcd6e8bf2
Donovan Graham <donovan@platform7.com>
2017-07-23 19:17:23 +0200

    fix: ssr

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+
+import App from '../client/App.jsx';
 
 const server = express();
 
@@ -8,6 +12,11 @@ const clientDistPath = path.join(__dirname, '../dist/client');
 const clientDistStaticPath = path.join(clientDistPath, 'static');
 
 server.use('/static', express.static(clientDistPath));
+
+server.use('/ssr', (req, res) => {
+  const html = renderToStaticMarkup(<App />);
+  res.send(html);
+});
 
 server.get('/', (req, res) => {
   const html = fs.readFileSync(path.join(clientDistPath, 'index.html'), 'utf-8');
